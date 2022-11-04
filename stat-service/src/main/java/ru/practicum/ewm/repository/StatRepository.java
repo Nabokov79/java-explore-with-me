039bc1e0da3e7cf4652ce112266aca3c5bb175d3
Nabokov79 <nabokovsg@yandex.ru>
2022-11-04 14:22:35 +0300

    add repository class

--- stat-service/src/main/java/ru/practicum/ewm/repository/StatRepository.java
+++ stat-service/src/main/java/ru/practicum/ewm/repository/StatRepository.java
@@ -1,14 +1,14 @@
 package ru.practicum.ewm.repository;
 
-import ru.practicum.ewm.model.EndpointHit;
+import ru.practicum.ewm.model.Stat;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface StatRepository extends JpaRepository<EndpointHit, Long> {
+public interface StatRepository extends JpaRepository<Stat, Long> {
 
-    @Query("select e from EndpointHit e where e.uri in :urisList")
-    List<EndpointHit> findAllByUri(@Param("urisList") List<String> urisList);
+    @Query("select e from Stat e where e.uri in :urisList")
+    List<Stat> findAllByUri(@Param("urisList") List<String> urisList);
 }
