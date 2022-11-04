98c5595788250e385c1e684d8f3c12c20d072c8d
Nabokov79 <nabokovsg@yandex.ru>
2022-11-04 11:52:28 +0300

    add model class

---
+++ ewm-service/src/main/java/ru/practicum/ewm/categories/model/Category.java
@@ -0,0 +1,22 @@
+package ru.practicum.ewm.categories.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name = "name", nullable = false)
+    private String name;
+}
