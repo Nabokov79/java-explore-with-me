2b604c58464eee6ca8357d164427ef63b06a192d
Nabokov79 <nabokovsg@yandex.ru>
2022-11-04 14:22:50 +0300

    add service class

--- ewm-service/src/main/java/ru/practicum/ewm/events/service/PublicEventsServiceImpl.java
+++ ewm-service/src/main/java/ru/practicum/ewm/events/service/PublicEventsServiceImpl.java
@@ -4,6 +4,7 @@ import ru.practicum.ewm.client.EventClient;
 import ru.practicum.ewm.events.mapper.EventMapper;
 import ru.practicum.ewm.client.EndpointHit;
 import ru.practicum.ewm.events.model.Event;
+import ru.practicum.ewm.paramRequest.Param;
 import ru.practicum.ewm.paramRequest.ParamUserRequest;
 import ru.practicum.ewm.events.repository.EventsRepository;
 import ru.practicum.ewm.exeption.NotFoundException;
@@ -18,9 +19,7 @@ import ru.practicum.ewm.events.dto.EventShortDto;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 @Service
@@ -28,7 +27,7 @@ public class PublicEventsServiceImpl implements PublicEventsService {
 
     private final EventsRepository repository;
     private final EventClient eventClient;
-    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private static final String MAIN_SERVICE = "MainService";
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
@@ -64,10 +63,6 @@ public class PublicEventsServiceImpl implements PublicEventsService {
             eventList = eventList.stream().filter(event -> event.getParticipantLimit() >= 0)
                                           .collect(Collectors.toList());
         }
-        Map<Long, Event> idsMap = eventList.stream().collect(Collectors.toMap(Event::getId, Function.identity()));
-        String ids = idsMap.entrySet().stream().map(entry -> entry.getKey() + " = " + entry.getValue())
-                .collect(Collectors.joining(";", "[","]"));
-        saveStat(ids, request);
         List<EventShortDto> eventShortDtoList = eventList.stream().map(EventMapper::toEventShortDto)
                                                                   .collect(Collectors.toList());
         switch (param.getSort()) {
@@ -83,6 +78,7 @@ public class PublicEventsServiceImpl implements PublicEventsService {
                 break;
             default:
         }
+        saveStat(request);
         return eventShortDtoList;
     }
 
@@ -90,19 +86,20 @@ public class PublicEventsServiceImpl implements PublicEventsService {
     public EventFullDto getFullInfoEvent(Long id, HttpServletRequest request) {
         Event event = repository.findById(id)
                 .orElseThrow(() -> new NotFoundException(String.format("Event not found by id=%s", id)));
-        saveStat(String.valueOf(event.getId()), request);
+        saveStat(request);
         EventFullDto eventFullDto = EventMapper.toEventFullDto(event);
-        eventFullDto.setViews(getStatByUri(List.of(request.getRequestURI())));
+        eventFullDto.setViews(getStatByUri(request.getRequestURI()));
         logger.info("Get event by id={}", id);
         return eventFullDto;
     }
 
-    private void saveStat(String ids, HttpServletRequest request) {
-        eventClient.saveStat(new EndpointHit(ids, MAIN_SERVICE, request.getRequestURI(),
-                request.getRemoteAddr(), LocalDateTime.now().format(DATE_TIME_FORMATTER)));
+    private void saveStat(HttpServletRequest request) {
+        eventClient.saveStat(new EndpointHit(MAIN_SERVICE, request.getRequestURI(),
+                                                           request.getRemoteAddr(),
+                                                           LocalDateTime.now().format(Param.DATE_TIME_FORMATTER)));
     }
 
-    private long getStatByUri(List<String> uri) {
+    private long getStatByUri(String uri) {
         String uris = String.join(",",uri);
         Object stat = eventClient.getStat("","", uris, false).getBody();
         long list = 0L;
--- stat-service/src/main/java/ru/practicum/ewm/service/StatServiceImpl.java
+++ stat-service/src/main/java/ru/practicum/ewm/service/StatServiceImpl.java
@@ -1,14 +1,14 @@
 package ru.practicum.ewm.service;
 
-import ru.practicum.ewm.dto.EndpointHitDto;
+import ru.practicum.ewm.dto.EndpointHit;
 import ru.practicum.ewm.dto.ViewStats;
-import ru.practicum.ewm.model.EndpointHit;
+import ru.practicum.ewm.mapper.StatsMapper;
+import ru.practicum.ewm.model.Stat;
 import ru.practicum.ewm.repository.StatRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDateTime;
 import java.util.*;
 import java.util.stream.Collectors;
@@ -16,7 +16,7 @@ import java.util.stream.Collectors;
 @Service
 public class StatServiceImpl implements StatService {
     private final StatRepository repository;
-    public static final DateTimeFormatter DATA_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
     @Autowired
@@ -25,30 +25,23 @@ public class StatServiceImpl implements StatService {
     }
 
     @Override
-    public void saveStat(EndpointHitDto endpointHit) {
-        List<EndpointHit> endpointHitList = new ArrayList<>();
-        logger.info("Save stat with param id={}", endpointHit.getId());
-        List<Long> ids = Arrays.stream(endpointHit.getId().split(","))
-                                                           .map(Long::valueOf)
-                                                           .collect(Collectors.toList());
-        for (Long id : ids) {
-            endpointHitList.add(new EndpointHit(id, endpointHit.getApp(), endpointHit.getUri(),
-                                                    endpointHit.getIp(), endpointHit.getTimestamp()));
-        }
-        repository.saveAll(endpointHitList);
+    public void saveStat(EndpointHit endpointHit) {
+        Stat stat = StatsMapper.toStat(endpointHit);
+        repository.save(stat);
+        logger.info("Save stat with param app={}", endpointHit.getApp());
     }
 
     @Override
     public List<ViewStats> getAllStats(String start, String end, String uris, Boolean unique) {
         List<String> urisList = Arrays.stream(uris.split(",")).collect(Collectors.toList());
-        List<EndpointHit> statListDb;
+        List<Stat> statListDb;
         if (start != null && end != null) {
-            LocalDateTime start1 = LocalDateTime.parse(start, DATA_TIME_FORMATTER);
-            LocalDateTime end1 = LocalDateTime.parse(end, DATA_TIME_FORMATTER);
+            LocalDateTime start1 = LocalDateTime.parse(start, StatsMapper.DATA_TIME_FORMATTER);
+            LocalDateTime end1 = LocalDateTime.parse(end, StatsMapper.DATA_TIME_FORMATTER);
             statListDb = repository.findAllByUri(urisList).stream()
-                    .filter(endpointHit -> LocalDateTime.parse(endpointHit.getTimestamp(), DATA_TIME_FORMATTER).isAfter(start1)
-                            && LocalDateTime.parse(endpointHit.getTimestamp(), DATA_TIME_FORMATTER).isBefore(end1))
-                    .collect(Collectors.toList());
+                                                       .filter(endpointHit -> endpointHit.getTimestamp().isAfter(start1)
+                                                                           && endpointHit.getTimestamp().isBefore(end1))
+                                                        .collect(Collectors.toList());
         } else {
             statListDb = repository.findAllByUri(urisList);
         }
@@ -57,7 +50,7 @@ public class StatServiceImpl implements StatService {
             statListDb = new ArrayList<>(new TreeSet<>(statListDb));
         }
         List<ViewStats> statList = new ArrayList<>();
-        for (EndpointHit val : statListDb) {
+        for (Stat val : statListDb) {
             long hits = statListDb.stream().filter(endpointHit -> endpointHit.getUri().equals(val.getUri())).count();
             statList.add(new ViewStats(val.getApp(), val.getUri(), hits));
         }
