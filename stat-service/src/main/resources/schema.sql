114d9559e50c2560a0bada88513c3f7bbf3f0cf3
Nabokov79 <nabokovsg@yandex.ru>
2022-11-04 11:41:42 +0300

    add schema file

---
+++ stat-service/src/main/resources/schema.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS STATS
+(
+    id          BIGINT,
+    app         VARCHAR,
+    uri         VARCHAR,
+    ip          VARCHAR,
+    timestamp   TIMESTAMP WITHOUT TIME ZONE
+);
