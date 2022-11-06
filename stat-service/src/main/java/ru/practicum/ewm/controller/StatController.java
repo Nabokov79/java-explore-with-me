798a438bff843c0acb5b149561c54892ae4bd2cf
Nabokov79 <nabokovsg@yandex.ru>
2022-11-06 22:24:09 +0300

    add controller class

--- stat-service/src/main/java/ru/practicum/ewm/controller/StatController.java
+++ stat-service/src/main/java/ru/practicum/ewm/controller/StatController.java
@@ -1,24 +1,20 @@
 package ru.practicum.ewm.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import ru.practicum.ewm.dto.EndpointHit;
 import ru.practicum.ewm.dto.ViewStats;
 import ru.practicum.ewm.service.StatService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class StatController {
 
     private final StatService service;
-
-    @Autowired
-    public StatController(StatService service) {
-        this.service = service;
-    }
 
     @PostMapping("/hit")
     public void saveStat(@RequestBody EndpointHit endpointHit) {
@@ -29,7 +25,7 @@ public class StatController {
     public ResponseEntity<List<ViewStats>> getAllStats(
                                                @RequestParam(value = "start") String start,
                                                @RequestParam(value = "end") String end,
-                                               @RequestParam(value = "uris") String uris,
+                                               @RequestParam(value = "uris") List<String> uris,
                                                @RequestParam(value = "unique", defaultValue = "false") Boolean unique) {
         return ResponseEntity.ok().body(service.getAllStats(start, end, uris, unique));
     }
