283c878fd99f5100b77231f89296732985b35682
Nabokov79 <nabokovsg@yandex.ru>
2022-11-07 22:05:17 +0300

    add client class

--- ewm-service/src/main/java/ru/practicum/ewm/client/EventClient.java
+++ ewm-service/src/main/java/ru/practicum/ewm/client/EventClient.java
@@ -8,23 +8,31 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @Service
 @Slf4j
 public class EventClient extends StatClient {
 
+    private final String serviceName;
+
     @Autowired
-    public EventClient(@Value("${stat-service.url}") String serverUrl, RestTemplateBuilder builder) {
+    public EventClient(@Value("${stat-service.url}") String serverUrl,
+                       @Value("${service.name}") String serviceName,
+                                                 RestTemplateBuilder builder) {
         super(
                 builder
                         .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                         .requestFactory(HttpComponentsClientHttpRequestFactory::new)
                         .build()
         );
+        this.serviceName = serviceName;
     }
 
-    public void saveStat(EndpointHit endpointHit) {
+    public void saveStat(String uri, String ip,  String timestamp) {
+        EndpointHit endpointHit = new EndpointHit(serviceName,uri,ip,timestamp);
         log.info("Received request to save stat app={}, uri={}, ip={}, timestamp={}",
                 endpointHit.getApp(), endpointHit.getUri(), endpointHit.getIp(), endpointHit.getTimestamp());
         post("/hit", endpointHit);
