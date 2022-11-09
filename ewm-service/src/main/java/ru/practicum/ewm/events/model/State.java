84cce392ffa2a29155d5778396fd83790ba0d9c6
Nabokov79 <nabokovsg@yandex.ru>
2022-11-09 21:14:15 +0300

    add model class

--- ewm-service/src/main/java/ru/practicum/ewm/events/model/State.java
+++ ewm-service/src/main/java/ru/practicum/ewm/events/model/State.java
@@ -1,22 +1,8 @@
 package ru.practicum.ewm.events.model;
 
-
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-
 public enum State {
 
     PENDING,
     PUBLISHED,
-    CANCELED;
-
-    public static Optional<State> from(String stringState) {
-        for (State state : values()) {
-            if (state.name().equalsIgnoreCase(stringState)) {
-                return Optional.of(state);
-            }
-        }
-        return Optional.empty();
-    }
+    CANCELED
 }
