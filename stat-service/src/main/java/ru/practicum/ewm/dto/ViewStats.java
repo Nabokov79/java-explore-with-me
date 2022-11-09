428e94e65736a6a8f8eabcb32a6bd5f7d13a1a24
Nabokov79 <nabokovsg@yandex.ru>
2022-11-09 21:04:34 +0300

    add dto class

--- stat-service/src/main/java/ru/practicum/ewm/dto/ViewStats.java
+++ stat-service/src/main/java/ru/practicum/ewm/dto/ViewStats.java
@@ -2,12 +2,17 @@ package ru.practicum.ewm.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.Map;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ViewStats {
+
 
     private String app;
     private String uri;
