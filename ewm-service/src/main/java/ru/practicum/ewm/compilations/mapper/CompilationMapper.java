0ae12b01ce98a390094ab1f7de0d63f80ae8363d
Nabokov79 <nabokovsg@yandex.ru>
2022-11-16 20:39:21 +0300

    add mapper class

--- ewm-service/src/main/java/ru/practicum/ewm/compilations/mapper/CompilationMapper.java
+++ ewm-service/src/main/java/ru/practicum/ewm/compilations/mapper/CompilationMapper.java
@@ -5,7 +5,10 @@ import lombok.NoArgsConstructor;
 import ru.practicum.ewm.compilations.dto.CompilationDto;
 import ru.practicum.ewm.compilations.dto.NewCompilationDto;
 import ru.practicum.ewm.compilations.model.Compilation;
+import ru.practicum.ewm.events.dto.EventFullDto;
 import ru.practicum.ewm.events.mapper.EventMapper;
+import ru.practicum.ewm.events.model.Event;
+import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,13 +23,19 @@ public class CompilationMapper {
     }
 
     public static CompilationDto toCompilationDto(Compilation compilation) {
-        return new CompilationDto(compilation.getId(),
-                compilation.getEvents().stream().map(EventMapper::toEventShortDto).collect(Collectors.toList()),
-                compilation.getPinned(),
-                compilation.getTitle());
+        CompilationDto compilationDto = new CompilationDto();
+        compilationDto.setId(compilation.getId());
+        compilationDto.setEvents(toListEvents(compilation.getEvents()));
+        compilationDto.setPinned(compilation.getPinned());
+        compilationDto.setTitle(compilation.getTitle());
+        return compilationDto;
     }
 
     public static List<CompilationDto> toListDto(List<Compilation> compilations) {
         return compilations.stream().map(CompilationMapper::toCompilationDto).collect(Collectors.toList());
+    }
+
+    private static List<EventFullDto> toListEvents(List<Event> events) {
+        return events.stream().map(event -> EventMapper.toEventFullDto(event, new HashMap<>())).collect(Collectors.toList());
     }
 }
--- stat-service/src/main/java/ru/practicum/ewm/mapper/StatsMapper.java
+++ stat-service/src/main/java/ru/practicum/ewm/mapper/StatsMapper.java
@@ -2,30 +2,32 @@ package ru.practicum.ewm.mapper;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
-import ru.practicum.ewm.dto.EndpointHit;
+import ru.practicum.ewm.dto.EndpointHitDto;
 import ru.practicum.ewm.model.App;
 import ru.practicum.ewm.model.Stat;
-
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class StatsMapper {
 
     public static final DateTimeFormatter DATA_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-    public static Stat toStat(EndpointHit endpointHitDto, App app) {
+    public static Stat toStat(EndpointHitDto endpointHit, String uris, App app) {
         Stat stat = new Stat();
         stat.setApp(app);
-        stat.setUri(endpointHitDto.getUri());
-        stat.setIp(endpointHitDto.getIp());
-        stat.setTimestamp(LocalDateTime.parse(endpointHitDto.getTimestamp(), DATA_TIME_FORMATTER));
+        stat.setUri(uris);
+        stat.setIp(endpointHit.getIp());
+        stat.setTimestamp(LocalDateTime.parse(endpointHit.getTimestamp(), DATA_TIME_FORMATTER));
         return stat;
     }
-
-    public static App toApp(String name) {
-        App app = new App();
-        app.setName(name);
-        return app;
+    public static List<Stat> toListStat(List<String> uris, EndpointHitDto endpointHitDto, App app) {
+        List<Stat> stats = new ArrayList<>();
+        for (String uri : uris) {
+            stats.add(toStat(endpointHitDto, uri,app));
+        }
+        return stats;
     }
 }
