fec69d56787cada6d017c94d3758f75149ce061d
Nabokov79 <nabokovsg@yandex.ru>
2022-11-30 17:23:56 +0300

    add controller class

---
+++ ewm-service/src/main/java/ru/practicum/ewm/users/controller/PrivateSubscriptionController.java
@@ -0,0 +1,42 @@
+package ru.practicum.ewm.users.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.ewm.events.dto.EventFullDto;
+import ru.practicum.ewm.users.dto.UserDto;
+import ru.practicum.ewm.users.service.PrivateSubscriptionService;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(path = "/subscriptions/users/{userId}")
+public class PrivateSubscriptionController {
+
+    private final PrivateSubscriptionService service;
+
+    @PostMapping("/subscriber/{subscriberId}/subscribe")
+    public ResponseEntity<List<UserDto>> subscribe(@PathVariable Long userId, @PathVariable Long subscriberId) {
+        return ResponseEntity.ok().body(service.subscribe(userId, subscriberId));
+    }
+
+    @PatchMapping("/subscriber/{subscriberId}/unsubscribe")
+    public ResponseEntity<String> unsubscribe(@PathVariable Long userId, @PathVariable Long subscriberId) {
+        service.unsubscribe(userId, subscriberId);
+        return ResponseEntity.ok("Подписка отменена");
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDto>> get(@PathVariable Long userId) {
+        return ResponseEntity.ok().body(service.get(userId));
+    }
+
+    @GetMapping("/subscriber/{subscriberId}/event")
+    public ResponseEntity<List<EventFullDto>> getEvents(@PathVariable Long userId, @PathVariable Long subscriberId,
+                            @PositiveOrZero @RequestParam(name = "from", defaultValue = "0", required = false) int from,
+                            @Positive @RequestParam(name = "size", defaultValue = "10", required = false) int size) {
+        return ResponseEntity.ok().body(service.getEvents(userId, subscriberId, from, size));
+    }
+}
